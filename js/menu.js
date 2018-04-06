ae22ce3b66ba74aa6777c73b0452d3708eba1c5a
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-04-06 03:21:35 -0500

    new pages and menu bug fixes, scroll-to-top

--- js/menu.js
+++ js/menu.js
@@ -1,39 +1,103 @@
 // MARK: Menu code
+
+// Tracks menu state
+var menuOpen = false;
+
+var divBarrier;
+var menuList;
+
 document.addEventListener("DOMContentLoaded", function(event) { 
     var menuStr = `
       <input type="checkbox" />
       <span class="menu" onclick="menuClicked()">
         <span class="hamburger"></span>
-      </span>
-      <ul>
-        <li>
-          <a href="index.html">HOME.</a>
-        </li>
-        <li>
-          <a href="about.html">ABOUT.</a>
-        </li>
-        <li>
-          <a href="#">FUN.</a>
-        </li>
-      </ul>`;
+      </span>`;
 
     // Add menu to the body
     var menu = document.createElement('label');
     menu.innerHTML = menuStr.trim();
     document.body.appendChild(menu);
+
+    divBarrier = document.createElement('div');
+    divBarrier.id = "barrier";
+    document.body.appendChild(divBarrier);
+
+    menuList = document.createElement('ul');
+    menuList.id = "menuList";
+    menuList.innerHTML = `
+      <li>
+        <a href="index.html">HOME.</a>
+      </li>
+      <li>
+        <a href="about.html">ABOUT.</a>
+      </li>
+      <li>
+        <a href="fun.html">FUN.</a>
+      </li>
+    `;
+    document.body.appendChild(menuList);
+
+    // Add button scroll-to-top, only on Desktop
+    if (window.innerWidth <= 1024) {
+        return;
+    }
+    var btn = document.createElement('img');
+    btn.classList.add("noModal");
+    btn.src = "images/up arrow.png";
+    btn.id = "upArrow";
+    btn.onclick = function() {
+        window.scroll({
+          top: 0, 
+          left: 0, 
+          behavior: 'smooth' 
+        });
+    };
+    document.body.appendChild(btn);
+
+    // Show scroll-to-top button
+    window.addEventListener("scroll", function() {
+        if (!menuOpen) {
+            if (window.pageYOffset > 200) {
+                btn.style.opacity = "1";
+                btn.style.visibility = "visible";
+            }
+            else {
+                btn.style.visibility = "hidden";
+                btn.style.opacity = "0";
+            }
+        }
+    }, false);
 });
 
 // Disable scrolling once menu is opened
-var menuOpen = false;
-
 function menuClicked() {
     menuOpen = !menuOpen;
     if (menuOpen) {
         disableMobileScrolling();
-        $.fn.fullpage.setAllowScrolling(false);
+        if (window.jQuery) { 
+            $.fn.fullpage.setAllowScrolling(false);
+        }
     } else {
         enableMobileScrolling();
-        $.fn.fullpage.setAllowScrolling(true);
+        if (window.jQuery) { 
+            $.fn.fullpage.setAllowScrolling(true);
+        }
+    }
+    toggleLabelsAndBarrier();
+}
+
+// Show labels and click barrier
+function toggleLabelsAndBarrier() {
+    if (menuOpen) {
+        divBarrier.style.visibility = "visible";
+
+        menuList.style.visibility = "visible";
+        menuList.style.opacity = 1;
+    } else {
+        divBarrier.style.visibility = "hidden";
+
+        menuList.style.opacity = 0;
+        menuList.style.visibility = "hidden";
     }
 }
 
