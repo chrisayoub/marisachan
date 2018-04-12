1a20bc8904b381b0a53d2d06fbe5013d86f396ff
Chris Ayoub <chris.ayoubtexas@gmail.com>
2018-04-12 01:12:17 -0500

    Mobile AOS

--- js/aosInit.js
+++ js/aosInit.js
@@ -1,5 +1,12 @@
 // Initalize AOS
 document.addEventListener("DOMContentLoaded", function(event) { 
+    // Mobile case
+    if (window.innerWidth < 900) {
+		var attribs = document.querySelectorAll('[data-aos-delay]');
+		for (var i = 0; i < attribs.length; i++) {
+			attribs[i].setAttribute("data-aos-delay", "0");
+		}
+    }
     AOS.init({
         duration: 1000,
         once: true
